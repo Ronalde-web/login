97c43f156fa74bf0d6c664c0a42897e85839c786
Ronalde <ronalderota@gmail.com>
2023-12-17 10:36:46 -0300

    update

--- telaLogin/wsgi.py
+++ telaLogin/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for telalogin project.
+WSGI config for telaLogin project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'telalogin.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'telaLogin.settings')
 
 application = get_wsgi_application()
